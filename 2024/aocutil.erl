72c7486960984118080e89087a4e1c3588ddb3e2
Wil Gaboury <wgaboury@gmail.com>
2024-12-08 20:45:34 -0500

    Fix day8

--- 2024/aocutil.erl
+++ 2024/aocutil.erl
@@ -2,13 +2,5 @@
 -export([read_lines/1]).
 
 read_lines(Filename) ->
-    case file:read_file(Filename) of
-        {ok, Data} -> 
-            Lines = binary:split(Data, <<"\r\n">>, [global]),
-            lists:map(
-                fun(BinaryLine) -> 
-                    binary_to_list(BinaryLine) 
-                end, 
-                Lines
-            )
-    end.
+    {ok, Data} = file:read_file(Filename),
+    string:tokens(erlang:binary_to_list(Data), "\n").
