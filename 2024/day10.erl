02fba7613d81b8d7767cdf19ce09369d9fead1ac
Wil Gaboury <wgaboury@gmail.com>
2024-12-10 18:33:41 -0500

    Finish day 10 part 1

--- 2024/day10.erl
+++ 2024/day10.erl
@@ -1,23 +1,25 @@
 -module(day10).
--export([part1/1]).
+-export([part1/0, part1/1]).
+
+-define(DEFAULT_INPUT_FILE, "day10_input.txt").
 
 parse_map(Filename) ->
-  Lines = aocutil:read_lines(Filename),
-  Width = length(lists:nth(1, Lines)),
-  Height = length(Lines),
-  MapArray = lists:foldl(
-      fun({I, E}, Data) ->
-        array:set(I-1, E, list_to_integer([Data]))
-      end,
-      array:new(Width*Height),
-      lists:enumerate(lists:flatten(Lines))
-  ),
-  {MapArray, Width, Height}
+    Lines = aocutil:read_lines(Filename),
+    Width = length(lists:nth(1, Lines)),
+    Height = length(Lines),
+    MapArray = lists:foldl(
+        fun({I, E}, Data) ->
+            array:set(I-1, list_to_integer([E]), Data)
+        end,
+        array:new(Width*Height),
+        lists:enumerate(lists:flatten(Lines))
+    ),
+    {MapArray, Width, Height}.
 
-pos_to_array_index({X, Y}, Width) -> X + Width * Y.
-array_index_to_pos(I, Width) -> {I div Width, I rem Width}.
+pos_to_array_index({X, Y}, Width) -> X + Y * Width.
+array_index_to_pos(I, Width) -> {I rem Width, I div Width}.
 
-get_map_value({X, Y}, {MapArray, Width, Height}) ->
+get_map_value({X, Y}, {MapArray, Width, _}) ->
     case X >= 0 andalso X < Width andalso Y >= 0 andalso Y < Width of
         true -> array:get(pos_to_array_index({X, Y}, Width), MapArray);
         false -> undefined
@@ -43,15 +45,14 @@ calculate_score([Cur|Rest], Map, Visited, Score) ->
         true -> calculate_score(Rest, Map, Visited, Score);
         false -> 
             {X,Y} = Cur,
-            CurValue = get_map_value(Cur)
-            {MapArray, Width, Height} = Map,
+            CurValue = get_map_value(Cur, Map),
             NewVisited = sets:add_element(Cur, Visited),
             NewScore = case CurValue == 9 of true -> Score+1; false -> Score end,
             PosDiffs = [{0,1}, {1,0}, {0,-1}, {-1,0}],
             VisitCandidates = lists:map(fun({Dx, Dy}) -> {X+Dx, Y+Dy} end, PosDiffs),
-            RemoveVisited = lists:filter(fun(P) -> not sets:is_element(P, NewVisited) end, VisitCandidates)
+            RemoveVisited = lists:filter(fun(P) -> not sets:is_element(P, NewVisited) end, VisitCandidates),
             IsPath = fun(Next) -> 
-                Value = get_map_value(Next),
+                Value = get_map_value(Next, Map),
                 case Value of
                     undefined -> false;
                     _ -> Value - CurValue == 1
@@ -61,9 +62,12 @@ calculate_score([Cur|Rest], Map, Visited, Score) ->
             calculate_score(Rest++Visit, Map, NewVisited, NewScore)
     end.
 
+calculate_rating() -> ok. 
+
+part1() -> part1(?DEFAULT_INPUT_FILE).
 part1(Filename) ->
-  Map = parse_map(Filename),
-  {MapArray, Width, Height} = Map,
-  Trailheads = find_value_positions(0, MapArray, Width),
-  Scores = lists:map(fun(Trailhead) -> calculate_score(Trailhead, Map) end, Trailheads),
-  lists:foldl(fun(Score, Sum) -> Score + Sum end, 0, Scores).
+    Map = parse_map(Filename),
+    {MapArray, Width, _} = Map,
+    Trailheads = find_value_positions(0, MapArray, Width),
+    Scores = lists:map(fun(Trailhead) -> calculate_score(Trailhead, Map) end, Trailheads),
+    lists:foldl(fun(Score, Sum) -> Score + Sum end, 0, Scores).
